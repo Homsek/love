a73c88dd1c7e07a6aa8e0a5dd2bfc7eef5a3fbf1
Homsek <99750997+Homsek@users.noreply.github.com>
2025-02-13 13:32:52 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+var typed = new Typed('.screen__title span', {
+   strings: [
+      "Сладкой",
+      "Прекрасной",
+      "Карамельной",
+      "Ламповой",
+      "Замечательной",
+      "Доброй",
+      "Изящной",
+      "Самой лучшей"
+   ],
+   typeSpeed: 80,
+   backSpeed: 80,
+   loop: true,
+   fadeOut: false,
+   fadeOutDelay: 2000,
+   showCursor: false,
+});
